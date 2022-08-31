db08d9e5bbd14ba742e6045b17de58560b100e9d
João Victor <joaoviccttor.dev@proton.me>
2022-08-31 19:02:53 -0400

    fist commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Portifólio pessoal
+<div align="center">
+  <img src="https://user-images.githubusercontent.com/109038802/185954382-24d66659-a938-48fe-9342-7a5d9bd8cc13.png" width="700" />
+ </div>
---
+++ script.js
@@ -0,0 +1,17 @@
+//EFEITO DE ROLAGEM
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+}).reveal(`
+#home,
+#home .texto-cabecalho,
+#home .btn-header,
+#about,
+#about .sobre-mim,
+#about .container-habilidades,
+#about .container-info, 
+#projects, 
+#projects .header-projetos,
+#projects .container-img,
+#contact`)
